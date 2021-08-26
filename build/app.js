e7f4b2ed9ea16c6a8768c329d3bba38d29406d89
Tuan_Hung <hoangtuanhung220901@gmail.com>
2021-08-26 10:24:09 +0700

    first

---
+++ build/app.js
@@ -0,0 +1,53 @@
+const flask = new CodeFlask('#code-1', {
+    language: 'C++',
+    lineNumbers: true,
+    // areaId: 'thing1',
+    // ariaLabelledby: 'header1',
+    //handleTabs: true,
+    readonly: true,
+  });
+
+  flask.updateCode(`L = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+print(L[2:7])
+# Prints ['c', 'd', 'e', 'f', 'g']
+  `);
+// const code2 = new CodeFlask('.test', {
+//     language: 'C++',
+//     lineNumbers: true,
+//     readonly: true,
+//     // areaId: 'thing1',
+//     // ariaLabelledby: 'header1',
+//     //handleTabs: true,
+//   });
+// code2.updateCode(`L = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+// print(L[-7:-2])
+// # Prints ['c', 'd', 'e', 'f', 'g']
+// `);
+const flask1 = new CodeFlask('#code-2', {
+  language: 'C++',
+  lineNumbers: true,
+  // areaId: 'thing1',
+  // ariaLabelledby: 'header1',
+  //handleTabs: true,
+  readonly: true,
+});
+
+flask1.updateCode(`L = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+print(L[2:7])
+# Prints ['c', 'd', 'e', 'f', 'g']
+hihi
+`);
+const flask2 = new CodeFlask('#code-3', {
+  language: 'C++',
+  lineNumbers: true,
+  // areaId: 'thing1',
+  // ariaLabelledby: 'header1',
+  //handleTabs: true,
+  readonly: true,
+});
+
+flask2.updateCode(`L = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+print(L[2:7])
+# Prints ['c', 'd', 'e', 'f', 'g']
+tada
+`);
